a5f4700b5d5abf78315f58d45f9a8d70a33bf64d
jordanl412 <jordanlazan@gmail.com>
2023-03-13 12:39:27 -0500

    Updated WelcmeScreen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,36 +1,39 @@
-import React from 'react';
+import React from "react";
 import './WelcomeScreen.css';
 
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
         (
-            <div className='WelcomeScreen'>
+            <div className="WelcomeScreen">
                 <h1>Welcome to the Meet app</h1>
                 <h4>
-                    Log in to see upcoming events around the world for full-stack developers
+                    Log in to see upcoming events around the world for
+                    full-stack
+                    developers
                 </h4>
-                    <div className='button_cont' align='center'>
-                        <div class='google-btn'>
-                            <div class='google-icon-wrapper'>
-                                <img 
-                                    class='google-icon'
-                                    src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-                                    o.svg'
-                                    alt='Google sign-in'
+                    <div className="button_cont" align="center">
+                        <div class="google-btn">
+                            <div class="google-icon-wrapper">
+                                <img
+                                    class="google-icon"
+                                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
+                                    o.svg"
+                                    alt="Google sign-in"
                                 />
                             </div>
                             <button onClick={() => { props.getAccessToken() }}
-                                rel='nofollow noopener'
-                                class='btn-text'
+                                rel="nofollow noopener"
+                                class="btn-text"
                             >
-                                <b>Sign in with Google</b>
+                                <b>Sign in with google</b>
                             </button>
                         </div>
                     </div>
-                    <a href='"https://jordanl412.github.io/meet/privacy.html'
-                    rel='nofollow noopener'
+                    <a
+                    href="https://jordanl412.github.io/meet/privacy.html"
+                    rel="nofollow noopener"
                     >
-                        Privacy policy
+                    Privacy policy
                     </a>
             </div>
         )
