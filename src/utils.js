15b56409eeeb486717882fa3ef1c28d09aacca66
jordanl412 <jordanlazan@gmail.com>
2023-03-18 13:54:27 -0400

    better colors on a white background (pie chart)

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const utils = {
-    colors: ['#ff7f50', '#6495ed', '#dc143c', '#e0ffff', '#e6e6fa', '#ffb6c1']
+    colors: ['#ff7f50', '#6495ed', '#dc143c', '#006400', '#daa520', '#483d8b']
 }
 
 export default utils
